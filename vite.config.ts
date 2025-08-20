d5bad3d68d5f7090ecd48563128de7a1635c1fd3
ThanapornChai <thanapornchai2402@gmail.com>
2025-08-20 22:31:46 +0700

    chore: setup github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/birthday-card/', // ← ใส่ชื่อ repo ของคุณ เช่น /birthday-card/
 })
